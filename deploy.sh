938304d87721d088685decb610823de582f83fdb
kirai <kirai.chang@ark88.net>
2026-06-30 16:07:17 +0800

    把一些設定放到deploy跟docker compose裡設定

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@
 # 填入 git source 的絕對路徑
 PROJECT_DIR="/path/to/trading"
 
+# 填入機敏設定
+export AUTH_JWT_SECRET="change-me"
+export FINMIND_API_KEY="your-finmind-api-key"
+
 docker compose \
   -f "$PROJECT_DIR/docker-compose.yml" \
   --project-directory "$PROJECT_DIR" \
